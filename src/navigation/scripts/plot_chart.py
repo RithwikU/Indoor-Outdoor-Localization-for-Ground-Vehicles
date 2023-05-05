6ef4ecb5e426c12cba676f9f4a2cecd16e6dc41a
Aadith Kumar <aadith.j@gmail.com>
2023-05-05 15:46:46 -0400

    final graphs done

--- src/navigation/scripts/plot_chart.py
+++ src/navigation/scripts/plot_chart.py
@@ -4,13 +4,17 @@ import matplotlib.pyplot as plt
 import csv
 
 # File names
-trial = "1";
+trial = "2";
 filtered_file = "gtsam_filtered_" + trial;
 smoothed_file = "gtsam_smoothed_" + trial;
-gt_file = "spline_ground_truth";
-lidar_file = "spline_lidar_mixed";
-odom_file = "spline_odom_drift";
-gps_file = "spline_gps";
+gt_file = "spline_ground_truth_b";
+lidar_file = "spline_lidar_mixed_b";
+odom_file = "spline_odom_drift_b";
+gps_file = "spline_gps_b";
+
+
+data = "fst"  # o -odom, f - filtered result, s - smoothed result, g - gps, t - ground truth, l - lidar
+title = "Comparision of Filtered Result to Smoothed Result"
 
 
 # Smoothed results data
@@ -93,7 +97,7 @@ with open(path_gps, 'r') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
         count = count + 1;
-        if count > 15 and count < 23:
+        if count > 13 and count < 27:
             x_gps.append(float(row[0]))
             y_gps.append(float(row[1]))
 
@@ -101,15 +105,33 @@ with open(path_gps, 'r') as csvfile:
 
 
 #Plot all these in one graph
-plt.plot(x_filtered, y_filtered, 'r-x',alpha=0.5, label='filtered estimate')
-plt.plot(x_smoothed, y_smoothed, 'g-x', alpha=0.5, label='smoothed estimate')
-plt.plot(x_odom, y_odom, 'y--x', alpha=0.5, label='odometery from robot')
-plt.plot(x_gps, y_gps, 'm--x', alpha=0.5, label='gps localization')
-plt.plot(x_gt, y_gt, 'b-x', alpha=0.5, label='ground truth')
-plt.plot(x_lidar, y_lidar, 'k--x', alpha=0.5, label='lidar localization')
-plt.xlabel('x location (m)')
-plt.ylabel('y location (m)')
-plt.title('Raw sensor estimates vs smoothed results')
+
+#if data contains the letter "s" then it is smoothed data
+if "t" in data:
+    plt.plot(x_gt, y_gt, 'k-', linewidth=2, label='ground truth')
+
+if "s" in data:
+    plt.plot(x_smoothed, y_smoothed, 'g--', alpha=1, label='smoothed estimate')
+
+if "f" in data:
+    plt.plot(x_filtered, y_filtered, 'r--',alpha=1, label='filtered estimate')
+
+if "l" in data:
+    plt.plot(x_lidar, y_lidar, '--', color=(1,0.75,0), alpha=1, label='lidar localization')
+
+if "o" in data:
+    plt.plot(x_odom, y_odom, 'c--', alpha=1, label='odometery from robot')
+
+if "g" in data:
+    plt.plot(x_gps, y_gps, 'm--', alpha=1, label='gps localization')
+
+
+
+
+
+plt.xlabel('X location (m)')
+plt.ylabel('Y location (m)')
+plt.title(title);
 
 plt.legend()
 plt.show()
