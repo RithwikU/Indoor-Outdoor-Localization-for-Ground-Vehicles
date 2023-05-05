df37698ab27fa676c4a0f74d7e184f1ac60f84f2
Rithwik Udayagiri <rithwik.math@gmail.com>
2023-05-05 12:10:00 -0400

    Edit docker file

--- Dockerfile
+++ Dockerfile
@@ -25,6 +25,10 @@ RUN apt-get install ros-melodic-teleop-twist-keyboard -y
 RUN apt-get install ros-melodic-roslint -y
 RUN apt-get install ros-melodic-xacro -y
 RUN apt-get install ros-melodic-robot-state-publisher -y
+RUN apt-get install ros-melodic-geodesy -y
+RUN apt-get install ros-melodic-pcl-ros  -y
+RUN apt-get install ros-melodic-nmea-msgs -y
+RUN apt-get install ros-melodic-libg2o -y
 
 # networking
 RUN apt-get install net-tools -y
@@ -34,16 +38,17 @@ RUN apt-get install nmap -y
 # serial
 RUN pip3 install pyserial
 
-COPY . /sick_ws/src/SickTiM
+RUN mkdir -p /ese650/src
 RUN source /opt/ros/melodic/setup.bash && \
-    cd sick_ws/ && \
+    cd ese650/ && \
     apt-get update --fix-missing && \
     rosdep install --from-paths src --ignore-src -r -y
 RUN echo "source /opt/ros/melodic/setup.bash" >> ~/.bash_aliases
 RUN echo "alias .s='source devel/setup.bash'" >> ~/.bash_aliases
-RUN source /opt/ros/melodic/setup.bash && \
-    cd sick_ws/ && \
-    catkin_make
+
+RUN echo "ROS_MASTER_URI=http://192.168.84.187:11311" >> ~/.bash_aliases
+RUN echo "ROS_IP=192.168.84.185" >> ~/.bash_aliases
+RUN source /opt/ros/melodic/setup.bash
 
 WORKDIR '/ese650/src'
 ENTRYPOINT ["/bin/bash"]
---
+++ src/navigation/scripts/spline.py
@@ -0,0 +1,53 @@
+from scipy.interpolate import InterpolatedUnivariateSpline
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+# Read in the waypoints
+x = []
+y = []
+
+path = "../waypoints/"
+
+with open(path+'waypoints_direct.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+
+    for row in reader:
+        x.append(float(row[0]))
+        y.append(float(row[1]))
+
+axes = np.linspace(1, len(x), len(x))
+
+# Interpolate the waypoints
+x_spline = InterpolatedUnivariateSpline(axes, x)
+y_spline = InterpolatedUnivariateSpline(axes, y)
+
+# Sample the interpolated waypoints
+number_of_samples = 50
+axes_sampled = np.linspace(1, len(x), number_of_samples)
+noise_sd = 0.05 # Standard deviation of the noise added to the spline
+
+drift_x = np.zeros(number_of_samples)
+drift_y = np.zeros(number_of_samples)
+drift_x[25:35] = 1.0 # Add a drift to the spline for some samples in the x direction
+# drift_y[25:35] = 1.0 # Add a drift to the spline for some samples in the y direction
+
+x_sampled = x_spline(axes_sampled) + np.random.normal(0, noise_sd, number_of_samples) + drift_x
+y_sampled = y_spline(axes_sampled) + np.random.normal(0, noise_sd, number_of_samples) + drift_y
+
+# Save the interpolated waypoints
+# with open(path+'dense_straight_only.csv', 'w') as csvfile:
+with open(path+'spline.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+
+    for i in range(len(x_sampled)):
+        writer.writerow([x_sampled[i], y_sampled[i]])
+
+# Plot the interpolated waypoints
+plt.plot(x_sampled, y_sampled, 'r-x', label='spline')
+plt.plot(x, y, 'b--x', alpha=0.5, label='waypoints')
+plt.legend()
+plt.show()
+
+
+
